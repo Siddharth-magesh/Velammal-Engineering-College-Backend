e755b526d98995eb1efe0c9784de92987278f4a9
4274-hari <hari.velusmy@gmail.com>
2025-07-11 12:02:38 +0530

    Edit test

--- database/test.py
+++ database/test.py
@@ -9,27 +9,6 @@ db_name = "NEW_VEC"
 client = MongoClient(mongo_uri)
 db = client[db_name]
 
-department_mapping1 = {
-    "Artificial Intelligence and Data Science (AI&DS)": "001",
-    "Automobile Engineering (AUTO)": "002",
-    "Chemistry": "003",
-    "Civil Engineering (CIVIL)": "004",
-    "Computer Science & Engineering (CSE)": "005",
-    "Computer Science and Engineering (CYBER SECURITY)": "006",
-    "Electrical & Electronics Engineering (EEE)": "007",
-    "Electronics & Instrumentation Engineering (EIE)": "008",
-    "Electronics and Communication Engineering (ECE)": "009",
-    "English": "010",
-    "Information Technology (IT)": "011",
-    "Mathematics": "012",
-    "Mechancial Engineering (MECH)": "013",
-    "Tamil": "014",
-    "Physics": "015",
-    "Master Of Computer Science": "016",
-    "Master of Business Admin": "017",
-    "Physical Education":"020",
-    "Placement":"021"
-}
 
 def insert_placement_data():
 
