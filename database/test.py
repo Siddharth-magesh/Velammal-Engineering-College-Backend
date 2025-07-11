3223a8338887666373a40d111c0393df6dd1cc2f
4274-hari <hari.velusmy@gmail.com>
2025-07-11 13:32:07 +0530

    YRC image changed expect 3

--- database/test.py
+++ database/test.py
@@ -9,14 +9,12 @@ db_name = "NEW_VEC"
 client = MongoClient(mongo_uri)
 db = client[db_name]
 
-
-def insert_placement_data():
-
-    collection = db['placements_data']  
-    with open("/root/Velammal-Engineering-College-Backend/docs/placements_data.json", "r",encoding="utf-8") as file:
+def insert_yrc_data():
+    collection = db["yrc_data"]
+    with open("/root/Velammal-Engineering-College-Backend/docs/yrc.json", "r",encoding="utf-8") as file:
         documents = json.load(file)
         collection.insert_many(documents)
 
-    print("placement documents inserted successfully.")
+    print("YRC data inserted successfully.")
 
-insert_placement_data()
+insert_yrc_data()
