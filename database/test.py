7f1e3d77fd3151d93f18e5a8f220859de8953049
4274-hari <hari.velusmy@gmail.com>
2025-07-15 15:44:37 +0530

    hods with dept_id

--- database/test.py
+++ database/test.py
@@ -11,12 +11,12 @@ db_name = "VEC"
 client = MongoClient(mongo_uri)
 db = client[db_name]
 
-def insert_library_data():
-    collection = db["library"]
-    with open("/root/Velammal-Engineering-College-Backend/docs/library.json", "r",encoding="utf-8") as file:
+
+def insert_hod_datas():
+    collection = db['HODS']
+    with open("/root/Velammal-Engineering-College-Backend/docs/hods.json", "r",encoding="utf-8") as file:
         documents = json.load(file)
         collection.insert_many(documents)
 
-    print("Library data inserted successfully.")
-
-insert_library_data()
+    print("HOD documents inserted successfully.")
+insert_hod_datas()
